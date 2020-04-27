0e6685a9c5abd0a00ce261945a064b5d640504ff
Norman <64245789+nmaneatspie@users.noreply.github.com>
2020-04-27 04:50:32 +0000

    Add files via upload

---
+++ mission1/compileC_findFromList.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set filename="findFromList"
+
+if exist %filename%.c goto compile
+if not exist %filename%.c goto error
+
+:compile
+
+cmd /c "gcc -o %filename% %filename%.c"
+@echo compilation complete
+pause
+exit
+
+:error
+@echo file %filename%.c does not exist
+pause
+exit
