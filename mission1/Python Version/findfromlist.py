9ff4d6b502b329d39b959a6b396f096256ee1d59
Norman T <nmaneats-pie@yahoo.com.au>
2020-10-25 20:28:24 +1100

    Added more code, and made a few changes to error_logging code. Used .rstrip to remove trailing characters from input files

--- mission1/Python Version/findfromlist.py
+++ mission1/Python Version/findfromlist.py
@@ -23,6 +23,7 @@ jumbled_file = "jumbled.txt"
 results_file = "results.txt"
 dictionary_words = []
 jumbled_words = []
+temp_words = []
 
 #Functions
 #File loading function
@@ -66,10 +67,9 @@ def error_log(err_num, filename = ""):
         sys.exit([2])
     err_msg = {
         1: "Error opening file {}".format(filename),
-        2: "Error with reading from {}".format(dictionary_file),
-        3: "Error with closing dictionary file {}".format(dictionary_file),
-        4: "Error reading from {}".format(jumbled_file),
-        5: "Error with closing dictionary file {}".format(jumbled_file),
+        2: "Error with reading from {}".format(filename),
+        3: "Error with closing {}".format(filename),
+        4: "Error writing to file {}".format(filename)
         }
     try:
         file = write_file(filename, -1)
@@ -87,25 +87,65 @@ file = load_file(dictionary_file, 1)
 
 try:
     for x in file: #Dictionary file has each word on it's own line
-        dictionary_words.append(x)
+        dictionary_words.append(x.rstrip())
 except:
-    error_log(2)
+    error_log(2, dictionary_file)
 
 try:
     file.close()
 except:
-    error_log(3)
+    error_log(3, dictionary_file)
 
 #Load jumbled words from file
 file = load_file(jumbled_file, 1)
 
 try:
     for x in file:
-        jumbled_words.append(x)
+        jumbled_words.append(x.rstrip())
 except:
-    error_log(4)
+    error_log(2, jumbled_file)
 
 try:
     file.close
 except:
-    error_log(5)
+    error_log(3, jumbled_file)
+
+if len(jumbled_words) == 0:
+    file = write_file(results_file, 4)
+    file.write("No jumbled words inputted")
+    file.close()
+    sys.exit([0])
+
+enabled = False
+
+if enabled == True: # To be done. This is the algorithm done in C converted to python
+    #reduce dictionary words to same length word temp list
+    for x in jumbled_words:
+        str_len = len(x)
+        for y in dictionary_words:
+            if len(y) == str_len:
+                temp_words.append(y)
+
+    temp2_words = [[]]
+    #reduce list further to words holding first char of jumbeld word
+    #for x in jumbled_words:
+
+
+else: # Practice to get more familiar with python workings
+    for x in jumbled_words: #jumbled_words string loop
+        for y in dictionary_words: #dictionary_words string loop
+            found = False
+            for i in x: #jumbled_words[x] char loop
+                found = False
+                for j in y: #dictionary_words[y] loop
+                    if j == i:
+                        found = True
+                        break
+                if found == True:
+                    continue
+                else:
+                    break
+            if found == True:
+                print("{}, Found: {}".format(x,y))
+                break
+print("end")
